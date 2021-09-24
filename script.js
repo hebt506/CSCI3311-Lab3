664ef5221565e9a56972bec5488749ee7fa3896d
Jinghong <hebt@bc.edu>
2021-09-24 13:24:20 -0400

    commit

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-
-d3.csv('cities.csv').then(data=>{
-	console.log('cities', data);
-})
+async function charting() {
+	
+	d3.csv('cities.csv', d=>{
+		return {
+		...d, // spread operator
+		eu: d.eu==='true', // convert to boolean
+		population: +d.population,
+		x: +d.x,
+		y: +d.y,
+		}
+	}).then(data=>{
+		console.log('cities', data.filter(d => d.eu === true));
+		d3.select('.city-count').text("There are" + data.length()+ "european cities")
+		const width = 700;
+		const height = 550;
+		const svg = d3.select('.population-plot')
+		.append('svg')
+    	.attr('width', width)
+   		.attr('height', height)
+	})	
+	
+} 
+	
+charting()
